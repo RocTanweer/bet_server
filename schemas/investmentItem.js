1522c54126cd3fec635e50128e2eb5d2054c45df
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-01-18 11:36:25 +0530

    added schema for investment items

---
+++ schemas/investmentItem.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'investmentItem',
+  title: 'Investment Item',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+  ],
+}
