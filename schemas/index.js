78a4ccd1f7f7c51bec37599e2cb277aeca33f1ff
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-02-23 22:38:30 +0530

    added media plugin | added User schema

--- schemas/index.js
+++ schemas/index.js
@@ -3,8 +3,10 @@ import investmentItem from './investmentItem'
 import saleItem from './saleItem'
 import saleTransaction from './saleTransaction'
 import business from './business'
+import user from './user'
 
 export const schemaTypes = [
+  user,
   investmentTransaction,
   investmentItem,
   saleTransaction,
---
+++ schemas/user.js
@@ -0,0 +1,39 @@
+export default {
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'firstName',
+      title: 'First Name',
+      type: 'string',
+    },
+
+    {
+      name: 'lastName',
+      title: 'Last Name',
+      type: 'string',
+    },
+
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+
+    {
+      name: 'password',
+      title: 'Password',
+      type: 'string',
+    },
+
+    {
+      name: 'profilePic',
+      title: 'Profile Pic',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+}
