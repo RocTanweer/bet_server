c9e67aeb1656f8c129c5e39dea2861f8d8149f5e
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-11-11 13:04:01 +0530

    MOD: added additional fields to business schema

--- schemas/business.js
+++ schemas/business.js
@@ -13,5 +13,31 @@ export default {
       title: 'Email',
       type: 'string',
     },
+    {
+      name: 'password',
+      title: 'Password',
+      type: 'string',
+    },
+    {
+      name: 'profilePicURL',
+      title: 'Profile Pic URL',
+      type: 'string',
+    },
+    {
+      name: 'profilePic',
+      title: 'Profile Pic',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'loginType',
+      title: 'Login Type',
+      type: 'string',
+      options: {
+        list: ['manual', 'gOAuth'],
+      },
+    },
   ],
 }
